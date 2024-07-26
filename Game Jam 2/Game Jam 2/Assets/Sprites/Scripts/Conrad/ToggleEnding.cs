d7e38e3350867e032967648786152af3a7ea7338
Yabu Darnoc <vanilla.icecream0919@gmail.com>
2024-07-26 22:38:47 +0800

    Ending Scenes Added

---
+++ Game Jam 2/Game Jam 2/Assets/Sprites/Scripts/Conrad/ToggleEnding.cs
@@ -0,0 +1,56 @@
+using LilLycanLord_Official;
+using UnityEngine;
+using UnityEngine.Assertions;
+
+namespace LilLycanLord_Official
+{
+    public class ToggleEnding : MonoBehaviour
+    {
+        //* ╔════════════╗
+        //* ║ Components ║
+        //* ╚════════════╝
+
+        //* ╔══════════╗
+        //* ║ Displays ║
+        //* ╚══════════╝
+        // [Header("Displays")]
+
+        //* ╔════════╗
+        //* ║ Fields ║
+        //* ╚════════╝
+        [Space(10)]
+        [Header("Fields")]
+        [SerializeField]
+        bool badEnd = false;
+
+        [SerializeField]
+        float delay = 3;
+
+        [SerializeField]
+        Canvas canvas;
+
+        //* ╔════════════╗
+        //* ║ Attributes ║
+        //* ╚════════════╝
+
+        //* ╔═══════════════╗
+        //* ║ Monobehaviour ║
+        //* ╚═══════════════╝
+        void Awake() { }
+
+        void Start()
+        {
+            canvas.gameObject.SetActive(false);
+            if (badEnd)
+                Invoke("toggle", delay);
+        }
+
+        //* ╔═════════════════════╗
+        //* ║ Non - Monobehaviour ║
+        //* ╚═════════════════════╝
+        public void toggle()
+        {
+            canvas.gameObject.SetActive(true);
+        }
+    }
+}
